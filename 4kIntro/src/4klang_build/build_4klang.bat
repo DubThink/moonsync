e3c913777a6edcc0255c8f62f2281bce206b641a
Ben W <benjaminwelsh2@gmail.com>
2018-11-02 20:41:17 -0400

    fixed issue where 4klang build wouldn't overwrite files

--- 4kIntro/src/4klang_build/build_4klang.bat
+++ 4kIntro/src/4klang_build/build_4klang.bat
@@ -3,5 +3,5 @@ CD /D %~dp0
 REM build the 4klang.obj
 .\nasm.exe -fwin32 -o "4klang.obj" 4klang.asm
 REM copy them to the source folder
-xcopy 4klang.h ..
-xcopy 4klang.obj ..
+xcopy 4klang.h .. /y
+xcopy 4klang.obj .. /y
