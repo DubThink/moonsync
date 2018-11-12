1686ca565b6241de0b9a7b48f163242a39e62780
Adam Lastowka <acl2920@g.rit.edu>
2018-11-11 20:19:56 -0500

    added basic SDF pipeline to shader

--- 4kIntro/src/fragment_shader.glsl
+++ 4kIntro/src/fragment_shader.glsl
@@ -1,4 +1,15 @@
+
+// TODO List: 
+// seperate stuff into files
+// Add to uniforms (lights, etc.)
+// Benjamin noise stuff
+// Everything
+
 #version 430
+
+// ----------------------------------------------------- //
+// Big globals
+// ----------------------------------------------------- //
 
 precision mediump float;
 
@@ -6,8 +17,13 @@ layout (location=0) uniform vec4 fpar[4];
 layout (location=0) out vec4 color;
 in vec2 p;
 
+vec2 resolution = vec2(1280, 720); // TODO: get this as a uniform
+
 #define TIME fpar[0].x
-vec2 resolution = vec2(1280, 720);
+
+// ----------------------------------------------------- //
+// Structs and stuff
+// ----------------------------------------------------- //
 
 struct Camera
 {
@@ -21,12 +37,89 @@ struct Camera
 	vec3 rayDir;
 };
 
+struct raymarchResult
+{
+	vec3 position;
+	vec3 diffuse_color;
+};
+
+// ----------------------------------------------------- //
+// SDF functions
+// ----------------------------------------------------- //
+
+// copy-pasted from IQ
+float opSmoothUnion( float d1, float d2, float k ) {
+    float h = clamp( 0.5 + 0.5*(d2-d1)/k, 0.0, 1.0 );
+    return mix( d2, d1, h ) - k*h*(1.0-h);
+}
+
+float sphereSDF(in vec3 v, in vec3 center, in float radius)
+{
+	return length(v - center) - radius;
+}
+
+float worldSDF(in vec3 v)
+{
+	float sdf_agg = 0.0;
+
+	float sphere1 = sphereSDF(v, vec3(2.0,0,0), 5.0);
+	sphere1 += sin(4.0 * v.x) * sin(4.0 * v.y) * sin(4.0 * v.z) * 0.25; // blobs on this sphere
+
+	float sphere2 = sphereSDF(v, vec3(-4.0, 0.0, 0.0), 3.0);
+
+	sdf_agg = opSmoothUnion(sphere1, sphere2, 3.0); // unionize the spheres to make a... pinecone
+
+	return sdf_agg;
+}
+
+// ----------------------------------------------------- //
+// Rendering
+// ----------------------------------------------------- //
+
+const int MAX_STEPS = 60;
+const float MIN_HIT_DIST = 0.001;
+
+// @param ray origin and ray direction
+raymarchResult worldMarch(in vec3 ro, in vec3 rd) {
+	
+	raymarchResult marched;
+	marched.diffuse_color = vec3(0,0,0); // "sky" color
+	
+	for(int i = 0; i < MAX_STEPS; i++) {
+		float samp = worldSDF(ro); // find SDF at current march position
+
+		// If SDF is low enough, handle the collision.
+		if(samp < MIN_HIT_DIST) {
+			marched.diffuse_color = vec3(1.0);
+			//break; // This, uhh, "wobbles" everything when it's active, and I don't know what to do about it. Working on that.
+					 // I tried putting the whole if() after the ro incrementation, but it didn't do anything.
+		}
+
+		// Step ray forwards by SDF
+		ro += rd*samp;
+	}
+	marched.position = ro;
+
+	return marched;
+};
+
+// Tetrahedron technique from http://iquilezles.org/www/articles/normalsSDF/normalsSDF.htm
+vec3 calcWorldNormal(in vec3 p)
+{
+    const float h = 0.0001;
+    const vec2 k = vec2(1,-1);
+    return normalize(k.xyy*worldSDF(p + k.xyy*h) +
+                     k.yyx*worldSDF(p + k.yyx*h) +
+                     k.yxy*worldSDF(p + k.yxy*h) +
+                     k.xxx*worldSDF(p + k.xxx*h));
+}
+
 Camera getCam()
 {
 	Camera cam;
 	vec3 lookAt = vec3(0, 0, 0);
 	
-	cam.position = vec3(cos(TIME/4.0)*10.0, 3.0, sin(TIME/4.0)*10.0);
+	cam.position = vec3(cos(TIME/2.0)*10.0, 3.0, sin(TIME/2.0)*10.0);
 	
 	// figure out camera space from position and lookAt
 	cam.up = vec3(0, 1, 0);
@@ -50,6 +143,11 @@ void main(void) {
 	Camera cam1 = getCam();
 
 	vec3 poos = cam1.rayDir;
+	raymarchResult testMarch = worldMarch(cam1.position, cam1.rayDir);
+	vec3 norm = calcWorldNormal(testMarch.position);
 
-	color = vec4(abs(vec3(sin(poos.x*50.0), sin(poos.y*50.0), sin(poos.z*50.0))), 1.0);
+	color += vec4(testMarch.diffuse_color*max(0, dot(norm, normalize(vec3(1.0, 1.0, 0.0))))*vec3(1.0, 1.0, 1.0), 1.0);
+	color += vec4(testMarch.diffuse_color*max(0, dot(norm, normalize(vec3(0.1, -1.0, 0.2))))*vec3(0.3, 0.3, 0.5), 0.0);
+	color += vec4(testMarch.diffuse_color*max(0, dot(norm, normalize(vec3(-0.9, 1.0, 0.2))))*vec3(0.8, 0.5, 0.2), 0.0);
+	//color = vec4(testMarch.diffuse_color, 1.0);
 }
--- 4kIntro/src/post_processing_shader.inl
+++ 4kIntro/src/post_processing_shader.inl
@@ -9,19 +9,12 @@ const char *post_processing_shader_glsl_pr =
  "layout(location=0)uniform vec4 fpar[4];"
  "layout(location=0)out vec4 color;"
  "uniform sampler2D inputTexture;"
- "in vec2 p;"
- "vec3 greenish()"
- "{"
-   "return vec3(.2,.9,.35);"
- "}\n"
- "#define time fpar[0].x\n"
+ "in vec2 p;\n"
+ "#define time fpar[0].x;\n"
  "void main()"
  "{"
-   "vec2 v=.5*p+.5,l=v-vec2(.5,.5);"
-   "vec3 f=greenish();"
-   "float t=.2+.1*cos(atan(l.y,l.x)*10.+20.*l.x+sin(time));"
-   "f*=smoothstep(t,t+.01,length(l));"
-   "color=vec4(f,1.);"
+   "vec2 v=.5*p+.5;"
+   "color=texture(inputTexture,v);"
  "}";
 
 #endif // POST_PROCESSING_SHADER_INL_
